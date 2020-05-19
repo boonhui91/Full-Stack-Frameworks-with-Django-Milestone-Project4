73e8963d95de906f840917d09724f43830490778
boonhui91 <boonhui.91@hotmail.com>
2020-05-19 14:19:38 +0000

    allauth setup

--- DiveEcom/urls.py
+++ DiveEcom/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import trips.views
 import vendors.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('trips/', trips.views.index),
     path('vendors/', vendors.views.index)
 ]
