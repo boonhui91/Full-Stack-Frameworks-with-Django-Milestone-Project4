c525275a5f7d9f63b10ec9940798c7c394a303c6
boonhui91 <boonhui.91@hotmail.com>
2020-05-20 07:37:41 +0000

    update models

--- trips/models.py
+++ trips/models.py
@@ -30,6 +30,7 @@ class Trip(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     accommodation = models.ForeignKey(Accom, on_delete=models.SET_NULL, null=True)
     vendors = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return 'DiveTrip: ' + self.location
@@ -38,8 +39,7 @@ class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     firstName = models.CharField(blank=False, max_length=30)
     lastName = models.CharField(blank=False, max_length=30)
+    wishlist = models.ManyToManyField(Trip)
 
     def __str__(self):
         return self.firstName + ' ' + self.lastName
-
-
