c2a0f898b3c3e5d7f9532f41629de6e22b88adc3
boonhui91 <boonhui.91@hotmail.com>
2020-05-21 14:37:22 +0000

    setup allauth base template

--- DiveEcom/settings.py
+++ DiveEcom/settings.py
@@ -66,7 +66,8 @@ ROOT_URLCONF = 'DiveEcom.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'templates', 'allauth')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
