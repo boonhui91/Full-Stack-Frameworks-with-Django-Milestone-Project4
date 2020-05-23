9773975e6eb490aa842aaab6b46ca71400ed2710
boonhui91 <boonhui.91@hotmail.com>
2020-05-23 18:56:02 +0000

    update total cost calculation when adjust qty

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect, reverse, HttpResponse
 from django.http import HttpResponseForbidden
 from trips.models import Trip
-import json
-import datetime
-from json import JSONEncoder
+
 
 # Create your views here.
 
@@ -22,6 +20,7 @@ def add_cart(request, trip_id):
             'qty': 1,
             'date': str(trip.date),
             'price': str(trip.price),
+            'original_price': str(trip.price),
             'image': str(trip.image)
 
         }
@@ -62,6 +61,15 @@ def delete_qty(request, trip_id):
         else:
             cart[trip_id]['qty'] -= 1
 
+            # get original price
+            original_price =float(cart[trip_id]['original_price'])
+            # convert last stored price from str to float
+            price_float = float(cart[trip_id]['price'])
+            # minus orginal price to last stored price when minus 1 qty
+            updated_price = (price_float - original_price)
+            # update latest price
+            cart[trip_id]['price'] = str(updated_price)
+
         request.session[SHOPPING_CART] = cart
 
     return redirect(reverse('view_cart_route'))
@@ -70,6 +78,16 @@ def add_qty(request, trip_id):
     cart = request.session.get(SHOPPING_CART)
     if trip_id in cart:
         cart[trip_id]['qty'] += 1
+        
+        # get original price
+        original_price =float(cart[trip_id]['original_price'])
+        # convert last stored price from str to float
+        price_float = float(cart[trip_id]['price'])
+        # adds orginal price to last stored price when plus 1 qty
+        updated_price = (price_float + original_price)
+        # update latest price
+        cart[trip_id]['price'] = str(updated_price)
+
         request.session[SHOPPING_CART] = cart
 
     return redirect(reverse('view_cart_route'))
@@ -78,5 +96,14 @@ def update_quantity(request, trip_id):
     cart = request.session.get(SHOPPING_CART)
     if trip_id in cart:
         cart[trip_id]['qty'] = int(request.POST['qty']) #converts update qty value to int, so that add and delete qty function can work
+
+        # get original price
+        original_price =float(cart[trip_id]['original_price'])
+        latest_qty = int(request.POST['qty'])
+        # original price multiply by updated qty
+        updated_price = (original_price * latest_qty)
+        # update latest price
+        cart[trip_id]['price'] = str(updated_price)
+
         request.session[SHOPPING_CART] = cart    
     return redirect(reverse('view_cart_route'))
