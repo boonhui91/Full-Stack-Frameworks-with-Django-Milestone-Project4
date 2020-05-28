1707d9476d00ec42a131e5f2453ea968f9183722
boonhui91 <boonhui.91@hotmail.com>
2020-05-28 07:49:34 +0000

    sending email

--- DiveEcom/settings.py
+++ DiveEcom/settings.py
@@ -105,7 +105,16 @@ LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 ACCOUNT_SIGNUP_FORM_CLASS = 'useraccount.forms.SignupForm'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if "DEVELOPMENT" in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 WSGI_APPLICATION = 'DiveEcom.wsgi.application'
 
